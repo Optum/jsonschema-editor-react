5e8ac0d82687bec66c59bd3e2b477c608036fcd2
Ryan Sites <ryan.sites@optum.com>
2021-06-01 11:44:58 -0400

    switch to react 17

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+const toPath = (_path) => path.join(process.cwd(), _path);
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+  ],
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+        },
+      },
+    };
+  },
+};
