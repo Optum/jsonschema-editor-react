9229dcac0bba53b5a718221c08f1a6d6bcee4d9b
Ryan Sites <ryan.sites@optum.com>
2021-06-03 11:57:29 -0400

    update docs

--- src/stories/JsonSchemaEditor.stories.tsx
+++ src/stories/JsonSchemaEditor.stories.tsx
@@ -2,30 +2,30 @@ import React from "react";
 
 import { Story, Meta } from "@storybook/react";
 
-import JsonSchemaEditor from "../JsonSchemaEditor";
-import { SchemaEditorProps } from "../JsonSchemaEditor/JsonSchemaEditor.types";
+import JsonSchemaEditor from "..";
+import { SchemaEditorProps } from "../JsonSchemaEditor.types";
 import { readOnlyData, printIt } from "./helper";
 
 export default {
-  title: "Example/SchemaEditor",
-  component: JsonSchemaEditor,
+	title: "Example/SchemaEditor",
+	component: JsonSchemaEditor,
 } as Meta;
 
 const Template: Story<SchemaEditorProps> = (args) => (
-  <JsonSchemaEditor {...args} />
+	<JsonSchemaEditor {...args} />
 );
 
 export const NewJsonSchema = Template.bind({});
 NewJsonSchema.args = {
-  onSchemaChange: (r) => {
-    console.log(r);
-  },
+	onSchemaChange: (r) => {
+		console.log(r);
+	},
 };
 
 export const WithData = Template.bind({});
 WithData.args = {
-  data: readOnlyData,
-  onSchemaChange: (r) => {
-    printIt(r);
-  },
+	data: readOnlyData,
+	onSchemaChange: (r) => {
+		printIt(r);
+	},
 };
